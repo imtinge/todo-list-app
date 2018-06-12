c81a90567587d7e92abd7bfd672434cacc8c5c34
imtinge <imtinge@outlook.com>
2018-06-12 16:23:13 +0800

    'ok'

---
+++ app/assets/javascripts/task.js
@@ -0,0 +1,36 @@
+class Task {
+    constructor(taskParams={}) {
+        Object.assign(this, taskParams)
+    }
+
+    save(callback) {
+        if (typeof(this.id) === 'undefined') {
+            this.create(callback)
+        } else {
+            this.update(callback)
+        }
+    }
+
+    create(callback) {
+        axios.post('/api/tasks', {
+            task: this
+        }).then(res => {
+            callback(res)
+        })
+    }
+
+    update(callback) {
+        axios.patch(`/api/tasks/${this.id}`, {
+            task: this
+        }).then(res => {
+            callback(res)
+        })
+    }
+
+    destroy(callback) {
+        axios.delete(`/api/tasks/${this.id}`)
+        .then(res => {
+            callback(res)
+        })
+    }
+}
